3297f693bdddfead37f6e65c879a7468c43915df
DESKTOP-NGMC3Q5\HP <jimmy990791@gmail.com>
2017-09-22 14:37:13 +0900

    붹

--- XmobileWebDesign/common/script/login.js
+++ XmobileWebDesign/common/script/login.js
@@ -1,6 +1,17 @@
 window.onload = function(){
     $("#login_form").on("submit",function(event){
         event.preventDefault();
-        var loginData = 
+        var loginData = $(this).serialize();
+        $.ajax({
+            url:"",
+            type:"POST",
+            data:loginData,
+            success:function(event){
+                console.log(event);
+            },
+            error:function(){
+                console.log("Error");
+            }
+        })
     });
 }
